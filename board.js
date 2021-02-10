c152510cefd836abeb3b9cb64895353292accce8
Natalie Oldroyd <natoh18@gmail.com>
2021-02-10 16:22:07 -0700

    Attack

--- board.js
+++ board.js
@@ -30,11 +30,11 @@ class Board {
           randomIndex++
         }
     }
-    //
-    grid[1][1] = "h";
-    grid[0][0] = "h";
-    grid[2][2] = "h";
-    //
+    // //
+    // grid[1][1] = "h";
+    // grid[0][0] = "h";
+    // grid[2][2] = "h";
+    // //
     return grid;
   }
 
@@ -85,20 +85,35 @@ class Board {
 
   isGameOver() {
     // TODO: Return true if the game is over (when all ships are hit).
+    return this.count() === 0;
   }
 
-  attack() {
+  attack(pos) {
     // TODO: Take in an attack position in the form of an array, [row, col], as
     // a parameter. Update this.grid depending on if the position is an empty
     // space or a damaged ship.
+    if (!this.isValidMove(pos)) return
+
+    if (this.grid[pos[0]][pos[1]] === "s") {
+      this.grid[pos[0]][pos[1]] = 'h'
+    } else {
+      this.grid[pos[0]][pos[1]] = 'x'
+    }
   }
 }
+
+
 
 module.exports = Board;
 
 
-let testBoard = new Board(3, 3, 3);
+let testBoard = new Board(3, 3, 4);
+testBoard.attack([0,0])
+testBoard.attack([1,1])
+testBoard.attack([2,2])
 testBoard.display();
 console.table(testBoard.grid);
+
 console.log(testBoard.count());
 console.log(testBoard.isValidMove([1,0]))
+console.log(testBoard.isGameOver())
