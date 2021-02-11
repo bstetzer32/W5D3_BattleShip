0d1ac9166afbac6aa0f7318a3e9416c9710b7129
Natalie Oldroyd <natoh18@gmail.com>
2021-02-10 17:01:42 -0700

    game

--- game.js
+++ game.js
@@ -16,18 +16,30 @@ class BattleshipGame {
 
   playTurn() {
     // TODO: Display the state of the game and ask for the users input.
+    this.board.display()
+    this.player1.getMove(this.player1.processMove)
   }
 
   displayStatus() {
     // TODO: Display the current state of the game to the player.
   }
 
-  processMove() {
+  processMove(pos) {
     // TODO: Detemerine if the move is valid. If so, invoke the attack method on
     //     the board instance and increment this.turns by 1. If the game is over,
     //     display the final status of the game and end the game. If not, play
     //     another turn. If the move is invalid, ask the player to input a valid
     //     position and play another turn.
+    if (this.board.isValidMove(pos)) {
+      this.board.attack(pos)
+      this.turns++
+    } else {
+      this.player1.getMove(this.player1.processMove)
+    }
+
+    if (this.board.isGameOver()) {
+      this.player1.processGameOver(true, this.turns)
+    }
   }
 }
 
